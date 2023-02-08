655510a0a8b1d78e265cf214c25f288ba9093382
DINOTICK <79216416+DINOTICK-Dev@users.noreply.github.com>
2023-02-08 15:12:01 -0500

    Added more stuff

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # SCP-Foundation
-A simple python application that can display information about a variety of scps
+A simple python application that can display information about a variety of SCPs
 
-WARNING! STILL IN DEVELOPMENT AND CURRENTLY IS BROKEN, AS THIS APPLICATION/TOOL IS STILL IN DEVELOPMENT AND IS NOT FINISHED
+WARNING! STILL IN DEVELOPMENT AND CURRENTLY BROKEN, AS THIS APPLICATION/TOOL IS STILL IN DEVELOPMENT AND IS NOT FINISHED
---
+++ func/scp.py
@@ -0,0 +1,18 @@
+import os, shutil, sys, re, urllib.request, pytest
+from math import ceil
+from bs4 import BeautifulSoup
+from typing import Union
+from tqdm import tqdm
+import requests
+
+def GetSCP(scpid,):
+  if scpid < 1000:
+    url = "http://scp-wiki.wikidot.com/scp-series"
+  elif scpid % 1000 == 0:
+      url = f"http://scp-wiki.wikidot.com/scp-series-{int(scpid/1000+1)}"
+  else:
+    url = f"http://scp-wiki.wikidot.com/scp-series-{ceil(scpid/1000, 0)}"
+    
+  r = requests.get(url)
+  print(r)
+
